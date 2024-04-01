af6ed3a2dc36d7d4a9b580c1bf31382a92abce2d
Ignacio <ignaciogimenez70@gmail.com>
2024-04-01 15:20:19 -0300

    refactor in repository

--- api/src/main/java/com/nancystore/api/repositories/UserRepository.java
+++ api/src/main/java/com/nancystore/api/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package com.nancystore.api.repositories;
 import com.nancystore.api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
